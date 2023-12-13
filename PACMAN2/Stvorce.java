7eb25feac070dedd4b6be5091cfebd1640d9a82f
Baltazar3334 <pltka1235@gmail.com>
2023-12-13 13:06:11 +0100

    nieco som musel komitnut stvorce alebo daco

--- PACMAN2/Stvorce.java
+++ PACMAN2/Stvorce.java
@@ -1,15 +1,7 @@
 
-/**
- * Write a description of class Stvorec here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 import fri.shapesge.Stvorec;
 public class Stvorce {
-    /**
-     * Constructor for objects of class Stvorec
-     */
+    
     private Stvorec stvorec;
     public Stvorce() {
         stvorec = new Stvorec();
