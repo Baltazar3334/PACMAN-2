2208369010354cb34d5cbd42d2cc63f9a0abb2ad
DominkoFRI <dominikpiencak@gmail.com>
2023-12-13 14:09:19 +0100

    piškotky

---
+++ PACMAN2/Piskot.java
@@ -0,0 +1,10 @@
+import fri.shapesge.Kruh;
+public class Piskot {
+    private Kruh piskot;
+    public Piskot() {
+        this.piskot = new Kruh();
+        this.piskot.zmenPriemer(5);
+        this.piskot.zmenFarbu("yellow");
+        this.piskot.zobraz();
+    }
+}
