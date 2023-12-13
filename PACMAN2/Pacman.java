6ef5b37a63fb18682bd1f758b0a86174f7e96c72
DominkoFRI <dominikpiencak@gmail.com>
2023-12-13 13:21:47 +0100

    Pacman

---
+++ PACMAN2/Pacman.java
@@ -0,0 +1,52 @@
+<<<<<<< HEAD
+import fri.shapesge.Obrazok;
+public class Pacman {
+    private Obrazok pacman;
+    private int x;
+    private int y;
+    private boolean pohyb;
+    public Pacman() {
+        this.pacman = new Obrazok("pic/pacman.png");
+        this.pacman.zobraz();
+        this.x = 0;
+        this.y = 0;
+        this.pohyb = false;
+    }
+    
+    public int getX() {
+        return this.x;
+    }
+    
+    public int getY() {
+        return this.y;
+    }
+    
+    public void pohybH() {
+        this.pacman.posunZvisle(this.y - 5);
+    }
+    
+    public void pohybD() {
+        this.pacman.posunZvisle(this.y + 5);
+    }
+    
+    public void pohybP() {
+        this.pacman.posunVodorovne(this.x + 5);
+    }
+    
+    public void pohybL() {
+        this.pacman.posunVodorovne(this.x - 5);
+    }
+    
+=======
+public class Pacman {
+    private int sirka;
+    private int vyska;
+    private boolean stav;
+    private int x;
+    private int y;
+    private int smer;
+    public Pacman() {
+        
+    }
+>>>>>>> 2142b3433571fe50da21daf5e77fff9a81ba7fd6
+}
